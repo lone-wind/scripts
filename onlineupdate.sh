8769787b984ef71c106157ceb42b52fb7155c052
lone-wind <82082609+lone-wind@users.noreply.github.com>
2021-07-21 13:01:21 +0800

    Update onlineupdate.sh

--- onlineupdate.sh
+++ onlineupdate.sh
@@ -1,7 +1,7 @@
 #---Edit by lone_wind
 #检查更新
 check_update () {
-    opkg update && opkg install gzip pv
+    opkg update && opkg install gzip
 }
 #工作目录
 work_path () {
@@ -211,7 +211,7 @@ version_confirm () {
 unzip_fireware () {
     rm -rf openwrt-rockchip-armv8-friendlyarm_nanopi-r${machine}s-${format}-sysupgrade.img
     echo -e '\e[92m开始解压固件\e[0m'
-    pv openwrt-rockchip-armv8-friendlyarm_nanopi-r${machine}s-${format}-sysupgrade.img.gz | gunzip -dc > openwrt-rockchip-armv8-friendlyarm_nanopi-r${machine}s-${format}-sysupgrade.img
+    gunzip -c -v openwrt-rockchip-armv8-friendlyarm_nanopi-r${machine}s-${format}-sysupgrade.img.gz
     if [ -f openwrt-rockchip-armv8-friendlyarm_nanopi-r${machine}s-${format}-sysupgrade.img	]; then
         echo -e '\e[92m已解压出升级文件\e[0m'
         firmware_check
