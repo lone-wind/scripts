47fe2d9fa7745948f381c1056831839e1e6b8b7c
lone-wind <82082609+lone-wind@users.noreply.github.com>
2021-09-03 20:10:37 +0800

    Update adg.sh

--- adg.sh
+++ adg.sh
@@ -218,6 +218,10 @@ adg_function () {
             ;;
         1)
             echo -e '\e[92m已选择：创建/更新 Adg容器\e[0m'${adg_num}
+            if [ ! -d "${save_path}/adg/workdir${adg_num}" ]; then
+                echo -e '\e[91mAdg工作目录不存在 请创建\e[0m'
+                change_path
+            fi
             build_adg
             ;;
         2)
