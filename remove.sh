493665ba3c7e8bfaea6b6daf4c7415396c553c34
lone-wind <82082609+lone-wind@users.noreply.github.com>
2021-11-20 09:10:21 +0800

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -3,6 +3,10 @@ opkg list_installed
 uninstall_overview_wireguard () {
     opkg remove wireguard
     opkg remove luci-i18n-wireguard-zh-cn
+    opkg remove luci-i18n-wireguard-pt-br
+    opkg remove luci-i18n-wireguard-ja
+    opkg remove luci-i18n-wireguard-ru
+    opkg remove luci-i18n-wireguard-sv
     opkg remove luci-app-wireguard
     opkg remove luci-proto-wireguard
     opkg remove wireguard-tools
@@ -11,6 +15,9 @@ uninstall_overview_wireguard () {
 uninstall_overview_mwan () {
     opkg remove luci-app-syncdial
     opkg remove luci-i18n-mwan3helper-zh-cn
+    opkg remove luci-i18n-mwan3-ru
+    opkg remove luci-i18n-mwan3-zh-tw
+    opkg remove luci-i18n-mwan3-ja
     opkg remove luci-i18n-mwan3-zh-cn
     opkg remove luci-app-mwan3helper
 	opkg remove luci-app-mwan3
@@ -45,11 +52,39 @@ uninstall_service_smartdns () {
 	opkg remove smartdns
 }
 uninstall_service_wol () {
+	opkg remove luci-i18n-wol-pl
+	opkg remove luci-i18n-wol-pt
+	opkg remove luci-i18n-wol-ro
+	opkg remove luci-i18n-wol-ru
+	opkg remove luci-i18n-wol-sk
+	opkg remove luci-i18n-wol-tr
+	opkg remove luci-i18n-wol-uk
+	opkg remove luci-i18n-wol-vi
+	opkg remove luci-i18n-wol-zh-tw
+	opkg remove luci-i18n-wol-pt-br
+	opkg remove luci-i18n-wol-ca
+	opkg remove luci-i18n-wol-cs
+	opkg remove luci-i18n-wol-de
+	opkg remove luci-i18n-wol-el
+	opkg remove luci-i18n-wol-es
+	opkg remove luci-i18n-wol-fr
+	opkg remove luci-i18n-wol-he
+	opkg remove luci-i18n-wol-hu
+	opkg remove luci-i18n-wol-it
+	opkg remove luci-i18n-wol-ja
+	opkg remove luci-i18n-wol-ms
+	opkg remove luci-i18n-wol-no
+	opkg remove luci-i18n-wol-sv
 	opkg remove luci-i18n-wol-zh-cn
 	opkg remove luci-app-wol
 	opkg remove wol
 }
 uninstall_service_wifischedule () {
+	opkg remove luci-i18n-wifischedule-pt-br
+	opkg remove luci-i18n-wifischedule-it
+	opkg remove luci-i18n-wifischedule-ja
+	opkg remove luci-i18n-wifischedule-ru
+	opkg remove luci-i18n-wifischedule-sv
     opkg remove luci-i18n-wifischedule-zh-cn
 	opkg remove luci-app-wifischedule
 	opkg remove wifischedule
@@ -77,6 +112,7 @@ uninstall_service_vlmcsd () {
 	opkg remove vlmcsd
 }
 uninstall_service_gost () {
+	opkg remove luci-i18n-gost-zh-cn
 	opkg remove luci-app-gost
 	opkg remove gost
 }
@@ -135,7 +171,11 @@ uninstall_service_passwall () {
     opkg remove luci-app-passwall
 }
 uninstall_service_nlbwmon () {
+	opkg remove luci-i18n-nlbwmon-ja
+	opkg remove luci-i18n-nlbwmon-ru
     opkg remove luci-i18n-nlbwmon-zh-cn
+    opkg remove luci-i18n-wrtbwmon-zh-cn
+    opkg remove luci-app-wrtbwmon
     opkg remove luci-app-nlbwmon
     opkg remove nlbwmon
 }
@@ -149,6 +189,26 @@ uninstall_service_pushbot () {
 uninstall_service_speederv2 () {
     opkg remove luci-app-speederv2
     opkg remove udpspeederv2
+}
+uninstall_service_shairplay () {
+	opkg remove luci-i18n-shairplay-pt-br
+	opkg remove luci-i18n-shairplay-ru
+	opkg remove luci-i18n-shairplay-ja
+	opkg remove luci-app-shairplay
+	opkg remove shairplay
+}
+uninstall_service_jd () {
+	opkg remove luci-app-jd-dailybonus
+}
+uninstall_service_aliyundrive () {
+	opkg remove luci-i18n-aliyundrive-webdav-zh-cn
+	opkg remove luci-app-aliyundrive-webdav
+	opkg remove aliyundrive-webdav
+}
+uninstall_service_mosdns () {
+	opkg remove luci-i18n-mosdns-zh-cn
+	opkg remove luci-app-mosdns
+	opkg remove mosdns
 }
 uninstall_nas_filebrowser () {
     opkg remove luci-i18n-filebrowser-zh-cn
@@ -177,11 +237,37 @@ uninstall_nas_amule () {
 	opkg remove amule
 }
 uninstall_nas_minidlna () {
+	opkg remove luci-i18n-minidlna-fr
+	opkg remove luci-i18n-minidlna-zh-tw
+	opkg remove luci-i18n-minidlna-he
+	opkg remove luci-i18n-minidlna-hu
+	opkg remove luci-i18n-minidlna-it
+	opkg remove luci-i18n-minidlna-ja
+	opkg remove luci-i18n-minidlna-ms
+	opkg remove luci-i18n-minidlna-no
+	opkg remove luci-i18n-minidlna-pt-br
+	opkg remove luci-i18n-minidlna-pl
+	opkg remove luci-i18n-minidlna-pt
+	opkg remove luci-i18n-minidlna-ro
+	opkg remove luci-i18n-minidlna-ru
+	opkg remove luci-i18n-minidlna-sk
+	opkg remove luci-i18n-minidlna-sv
+	opkg remove luci-i18n-minidlna-tr
+	opkg remove luci-i18n-minidlna-uk
+	opkg remove luci-i18n-minidlna-vi
+	opkg remove luci-i18n-minidlna-ca
+	opkg remove luci-i18n-minidlna-cs
+	opkg remove luci-i18n-minidlna-de
+	opkg remove luci-i18n-minidlna-el
+	opkg remove luci-i18n-minidlna-es
     opkg remove luci-i18n-minidlna-zh-cn
 	opkg remove luci-app-minidlna
 	opkg remove minidlna
 }
 uninstall_nas_rclone () {
+	opkg remove rclone-webui-react
+	opkg remove rclone-ng
+	opkg remove rclone-config
     opkg remove luci-i18n-rclone-zh-cn
 	opkg remove luci-app-rclone
 	opkg remove rclone
@@ -192,6 +278,9 @@ uninstall_nas_qBittorrent () {
 	opkg remove qbittorrent
 }
 uninstall_nas_mjpg_streamer () {
+	opkg remove luci-i18n-mjpg-streamer-pt-br
+	opkg remove luci-i18n-mjpg-streamer-ja
+	opkg remove luci-i18n-mjpg-streamer-ru
     opkg remove luci-i18n-mjpg-streamer-zh-cn
 	opkg remove luci-app-mjpg-streamer
 	opkg remove mjpg-streamer
@@ -207,6 +296,29 @@ uninstall_nas_p910nd () {
 }
 uninstall_nas_transmission () {
 	opkg remove luci-i18n-transmission-zh-cn
+	opkg remove luci-i18n-transmission-ms
+	opkg remove luci-i18n-transmission-no
+	opkg remove luci-i18n-transmission-pl
+	opkg remove luci-i18n-transmission-pt
+	opkg remove luci-i18n-transmission-ro
+	opkg remove luci-i18n-transmission-ru
+	opkg remove luci-i18n-transmission-sk
+	opkg remove luci-i18n-transmission-sv
+	opkg remove luci-i18n-transmission-tr
+	opkg remove luci-i18n-transmission-uk
+	opkg remove luci-i18n-transmission-vi
+	opkg remove luci-i18n-transmission-zh-tw
+	opkg remove luci-i18n-transmission-ca
+	opkg remove luci-i18n-transmission-cs
+	opkg remove luci-i18n-transmission-de
+	opkg remove luci-i18n-transmission-el
+	opkg remove luci-i18n-transmission-es
+	opkg remove luci-i18n-transmission-fr
+	opkg remove luci-i18n-transmission-he
+	opkg remove luci-i18n-transmission-hu
+	opkg remove luci-i18n-transmission-it
+	opkg remove luci-i18n-transmission-pt-br
+	opkg remove luci-i18n-transmission-ja
 	opkg remove luci-app-transmission
 	opkg remove transmission-web-control
 	opkg remove transmission-daemon
@@ -227,11 +339,38 @@ uninstall_nas_cifsmount () {
 	opkg remove cifsmount
 }
 uninstall_nas_hd_idle () {
+	opkg remove luci-i18n-hd-idle-ca
+	opkg remove luci-i18n-hd-idle-cs
+	opkg remove luci-i18n-hd-idle-de
+	opkg remove luci-i18n-hd-idle-el
+	opkg remove luci-i18n-hd-idle-es
+	opkg remove luci-i18n-hd-idle-fr
+	opkg remove luci-i18n-hd-idle-zh-tw
+	opkg remove luci-i18n-hd-idle-he
+	opkg remove luci-i18n-hd-idle-hu
+	opkg remove luci-i18n-hd-idle-it
+	opkg remove luci-i18n-hd-idle-ja
+	opkg remove luci-i18n-hd-idle-ms
+	opkg remove luci-i18n-hd-idle-no
+	opkg remove luci-i18n-hd-idle-pl
+	opkg remove luci-i18n-hd-idle-pt
+	opkg remove luci-i18n-hd-idle-pt-br
+	opkg remove luci-i18n-hd-idle-ro
+	opkg remove luci-i18n-hd-idle-ru
+	opkg remove luci-i18n-hd-idle-sk
+	opkg remove luci-i18n-hd-idle-sv
+	opkg remove luci-i18n-hd-idle-tr
+	opkg remove luci-i18n-hd-idle-uk
+	opkg remove luci-i18n-hd-idle-vi
 	opkg remove luci-i18n-hd-idle-zh-cn
 	opkg remove luci-app-hd-idle
 	opkg remove hd-idle
 }
 uninstall_nas_aria2 () {
+	opkg remove luci-i18n-aria2-zh-tw
+	opkg remove luci-i18n-aria2-ru
+	opkg remove luci-i18n-aria2-sv
+	opkg remove luci-i18n-aria2-pt-br
     opkg remove luci-i18n-aria2-zh-cn
     opkg remove luci-app-aria2
     opkg remove ariang
@@ -290,6 +429,29 @@ uninstall_vpn_zerotier () {
 	opkg remove zerotier
 }
 uninstall_system_sqm () {
+	opkg remove luci-i18n-sqm-ca
+	opkg remove luci-i18n-sqm-cs
+	opkg remove luci-i18n-sqm-de
+	opkg remove luci-i18n-sqm-el
+	opkg remove luci-i18n-sqm-es
+	opkg remove luci-i18n-sqm-fr
+	opkg remove luci-i18n-sqm-he
+	opkg remove luci-i18n-sqm-hu
+	opkg remove luci-i18n-sqm-it
+	opkg remove luci-i18n-sqm-ja
+	opkg remove luci-i18n-sqm-ko
+	opkg remove luci-i18n-sqm-ms
+	opkg remove luci-i18n-sqm-pl
+	opkg remove luci-i18n-sqm-pt
+	opkg remove luci-i18n-sqm-ro
+	opkg remove luci-i18n-sqm-ru
+	opkg remove luci-i18n-sqm-sk
+	opkg remove luci-i18n-sqm-sv
+	opkg remove luci-i18n-sqm-tr
+	opkg remove luci-i18n-sqm-zh-tw
+	opkg remove luci-i18n-sqm-uk
+	opkg remove luci-i18n-sqm-vl
+	opkg remove luci-i18n-sqm-vi
 	opkg remove luci-i18n-sqm-zh-cn
 	opkg remove luci-app-sqm
 }
@@ -322,13 +484,16 @@ uninstall_plugin () {
     uninstall_service_adbyby
     uninstall_service_adguardhome
     uninstall_service_airplay2
+    uninstall_service_aliyundrive
     uninstall_service_familycloud
     uninstall_service_frp
     uninstall_service_godproxy
     uninstall_service_gost
     uninstall_service_guest_wifi
     uninstall_service_haproxy
+    uninstall_service_jd
     uninstall_service_kcptun
+    uninstall_service_mosdns
     uninstall_service_nlbwmon
     uninstall_service_nps
     uninstall_service_openclash
