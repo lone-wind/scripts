5acee1dd31f2163a3ce243656a9cf248c0dcd46c
lone-wind <82082609+lone-wind@users.noreply.github.com>
2022-02-10 16:15:11 +0800

    Update incr.sh

--- incr.sh
+++ incr.sh
@@ -12,9 +12,9 @@ hd_check () {
 #检测分区
 part_check () {
     if fdisk -l | grep -q /dev/${part_id}; then
-        continue;
+        echo -e '\e[91m分区存在\e[0m'
     else
-        part_incr
+        echo -e '\e[92m新建分区\e[0m' && part_incr
         mkfs.ext4 -F /dev/${part_id}
     fi
 }
@@ -22,9 +22,11 @@ part_check () {
 part_incr () {
     block_num=$(fdisk -l | grep /dev/${hd_id}p2 | awk '{print $3}')
     fdisk /dev/${hd_id} <<EOF
+    p
     n
     3
-    $($block_num+1)
+    $(($block_num+1))
+
     w
 EOF
 }
@@ -33,10 +35,13 @@ file_check () {
     if [ ! -d /mnt/${part_id} ]; then
         mkdir /mnt/${part_id}
     elif cat /proc/mounts | grep /dev/${part_id}; then
-        /etc/init.d/dockerd stop
+        /etc/init.d/dockerd restart && /etc/init.d/dockerd stop
         umount /dev/${part_id}
     fi
     mount /dev/${part_id} /mnt/${part_id}
+    if [ ! -d /mnt/${part_id}/docker ]; then
+        mkdir /mnt/${part_id}/docker
+    fi 
 }
 #检测容器
 docker_check () {
@@ -47,13 +52,12 @@ docker_check () {
             sed -i 's?/opt?/mnt/mmcblk0p3?' /etc/config/dockerd
         fi
     fi
-    /etc/init.d/dockerd start
+    /etc/init.d/dockerd start && /etc/init.d/dockerd restart
 }
 #程序开始
 incr_begin () {
 hd_check
 file_check
 docker_check
-df -h | grep /dev/${hd_id}
 }
 incr_begin
