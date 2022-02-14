66f57f5a0de3987544dbb7cc05ce16004f743d89
lone-wind <82082609+lone-wind@users.noreply.github.com>
2022-02-14 22:24:18 +0800

    Update incr.sh

--- incr.sh
+++ incr.sh
@@ -3,9 +3,11 @@
 rm -rf incr.sh
 #检测硬盘
 hd_check () {
-    hd_id='mmcblk0' && part_id='mmcblk0p3'
+    hd_id='mmcblk0'
+    part_id='mmcblk0p3'
     if [ ! -d /sys/block/$hd_id ]; then
-        hd_id='sda' && part_id='sda3'
+        hd_id='sda'
+        part_id='sda3'
     fi
     part_check
 }
@@ -14,7 +16,8 @@ part_check () {
     if fdisk -l | grep -q /dev/${part_id}; then
         echo -e '\e[92m分区正常\e[0m'
     else
-        echo -e '\e[91m分区异常，开始修改\e[0m' && part_incr
+        echo -e '\e[91m分区异常，开始修改\e[0m'
+        part_incr
     fi
 }
 #新建分区
@@ -30,17 +33,23 @@ EOF
 }
 #检测文件
 file_check () {
-    if cat /proc/mounts | grep /mnt/${part_id}\ ext4; then
+    if cat /proc/mounts | grep -q /mnt/${part_id}\ ext4; then
         echo -e '\e[92m新挂载点正常\e[0m'
         continue;
     else
         echo -e '\e[91m新挂载点异常，开始修改\e[0m'
-        /etc/init.d/dockerd start && /etc/init.d/dockerd stop
-        umount /dev/${part_id} && mkfs.ext4 -F /dev/${part_id}
-        if [ ! -d /mnt/${part_id} ] && [ ! -d /mnt/${part_id}/docker ]; then
-            mkdir /mnt/${part_id} && mkdir /mnt/${part_id}/docker
+        /etc/init.d/dockerd start
+        /etc/init.d/dockerd stop
+        if cat /proc/mounts | grep -q "/dev/${part_id}"; then
+            umount /dev/${part_id}
         fi
-        mount /dev/${part_id} /mnt/${part_id} && /etc/init.d/dockerd start
+        mkfs.ext4 -F /dev/${part_id}
+        if [ ! -d /mnt/${part_id} ] && [ ! -d /mnt/${part_id}/docker ]; then
+            mkdir /mnt/${part_id}
+            mkdir /mnt/${part_id}/docker
+        fi
+        mount /dev/${part_id} /mnt/${part_id}
+        /etc/init.d/dockerd start
     fi
 }
 #检测容器
@@ -52,14 +61,19 @@ docker_check () {
         else
             echo -e '\e[91mDocker根目录异常，开始修改\e[0m'
             sed -i 's?/opt?/mnt/mmcblk0p3?' /etc/config/dockerd
-            /etc/init.d/dockerd start && /etc/init.d/dockerd restart
+            /etc/init.d/dockerd start
+            /etc/init.d/dockerd restart
         fi
     fi
 }
 #程序开始
 incr_begin () {
-hd_check && file_check && docker_check && sleep 3
+hd_check
+file_check
+docker_check
+sleep 5
 echo -e '\e[92m脚本结束，请查看分区\e[0m'
 df -h | grep /dev/${part_id}
 }
+
 incr_begin
