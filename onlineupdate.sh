d8f447722c5aef07486b5563ae8d3fd3655f2c6a
lone-wind <82082609+lone-wind@users.noreply.github.com>
2023-01-06 16:51:42 +0800

    Update onlineupdate.sh

--- onlineupdate.sh
+++ onlineupdate.sh
@@ -122,7 +122,8 @@ repo_set () {
 search_file () {
     cd ${work_path} && clean_up && days=$(($days+1))
     #echo `(date -d "@$(($(busybox date +%s) - 86400*($days-1)))" +%Y.%m.%d)`
-    wget -q ${proxy_url}/${repo_url}/download/$(date -d "@$(($(busybox date +%s) - 86400*($days-1)))" +%Y.%m.%d)-Lean${machine_id}/${version_num}-sha256sums
+    #wget -q ${proxy_url}/${repo_url}/download/$(date -d "@$(($(busybox date +%s) - 86400*($days-1)))" +%Y.%m.%d)-Lean${machine_id}/${version_num}-sha256sums
+    wget -q ${repo_url}/download/$(date -d "@$(($(busybox date +%s) - 86400*($days-1)))" +%Y.%m.%d)-Lean${machine_id}/${version_num}-sha256sums
     exist_judge
 }
 #存在判断
@@ -143,7 +144,8 @@ firmware_confirm () {
     case $skip in
         [yY][eE][sS]|[yY])
             echo -e '\e[92m已确认，开始下载固件\e[0m'
-            wget ${proxy_url}/${repo_url}/download/$(date -d "@$(($(busybox date +%s) - 86400*($days-1)))" +%Y.%m.%d)-Lean${machine_id}/${firmware_url}.gz
+            #wget ${proxy_url}/${repo_url}/download/$(date -d "@$(($(busybox date +%s) - 86400*($days-1)))" +%Y.%m.%d)-Lean${machine_id}/${firmware_url}.gz
+            wget ${repo_url}/download/$(date -d "@$(($(busybox date +%s) - 86400*($days-1)))" +%Y.%m.%d)-Lean${machine_id}/${firmware_url}.gz
             ;;
         [nN][oO]|[nN])
             echo -e '\e[91m寻找前一天的固件\e[0m' && search_file
